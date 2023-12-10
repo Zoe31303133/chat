4a7d976e828e2a2c6a53664f6ca8a34383dd7b5d
Zoe31303133 <ch10161016@gmail.com>
2023-12-10 20:50:26 +0800

    登出:清除session

--- logOut/index.php
+++ logOut/index.php
@@ -1,6 +1,13 @@
 <?php
-
 session_start();
 
-$_SESSION['id']="id";
+var_dump($_SESSION);
+
+require_once('../chatroom/change_user_status.php');
+
+$uid = $_SESSION['uid'];
+
+session_unset();
+
+change_user_status("offline", $uid);
 ?>
