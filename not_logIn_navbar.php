2c77baa682fa76f33c3025e9c57e0af307f46ef3
Zoe31303133 <ch10161016@gmail.com>
2024-04-22 14:07:49 +0800

    user/ guest version navbar

---
+++ not_logIn_navbar.php
@@ -0,0 +1,15 @@
+
+<div class="navbar">
+    <div class="navbar_logo btn btn-primary">
+        logo
+    </div>
+
+    <div class="navbar_control">
+        <div class="navbar_control_user">
+                <a id="logout_btn" class="navbar_btn btn btn-outline-secondary" href="http://localhost:4000/signUp">註冊</a>
+                <a id="logout_btn" class="navbar_btn btn btn-outline-secondary" href="http://localhost:4000/logIn">登入</a>
+        </div>
+    </div>
+</div>
+
+    
