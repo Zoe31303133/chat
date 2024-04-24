d46166a87dc1570832ba2a9d0c8e94d3f57c8dba
Zoe31303133 <ch10161016@gmail.com>
2024-04-24 22:29:12 +0800

    [修正] 清除客戶端Session

--- logOut/index.php
+++ logOut/index.php
@@ -5,8 +5,10 @@
     require_once('../chatroom/change_user_status.php');
 
     $uid = $_SESSION['uid'];
+
     change_user_status("offline", $uid);
 
     session_destroy();
-
+    $_SESSION = [];
+    
 ?>
