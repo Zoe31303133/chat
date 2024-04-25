c005a7394962f5fe955a17fcdc46d7e88942b732
Zoe31303133 <ch10161016@gmail.com>
2024-04-25 22:38:55 +0800

    [修正] function參數引用錯誤導致失效

--- chatroom/change_user_status.php
+++ chatroom/change_user_status.php
@@ -6,7 +6,7 @@ function change_user_status($status, $uid){
     $conn = connection();
     $stmt = mysqli_stmt_init($conn);
     mysqli_stmt_prepare($stmt, $sql);
-    mysqli_stmt_bind_param($stmt, 'ss', $status, $my_uid);
+    mysqli_stmt_bind_param($stmt, 'ss', $status, $uid);
     mysqli_execute($stmt);
     mysqli_stmt_close($stmt);
     mysqli_close($conn);
