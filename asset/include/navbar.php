41156efcbac31e3ae99d67fa4ea3d7c368234d80
Zoe31303133 <ch10161016@gmail.com>
2024-05-01 10:51:51 +0800

    [整理] 將logo與navbar整理入asset/include

--- asset/include/navbar.php
+++
@@ -1,6 +0,0 @@
-<div class="navbar">
-    <!-- <div class="navbar_btn_wrapper"> -->
-    <button class="navbar_btn">註冊</button>
-    <button class="navbar_btn">登入</button>
-    <!-- </div> -->
-</div>
